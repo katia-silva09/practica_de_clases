0eff03b56f71ad1dcb0d1b0ea179bf61c165dcb0
DanielMRX777 <140866516+katia-silva09@users.noreply.github.com>
2023-10-25 21:11:34 -0600

    segunda parte
    termanindo segunda parte de completar el index.js

--- Js/index.js
+++ Js/index.js
@@ -12,6 +12,36 @@ function onCtickAdd() {
     botonAgregar.disabled = true;
 }
     function onTypeList() {
-    botonAgregar.disabled = input. value.length==0}
+    botonAgregar.disabled = input. value.length==0;
+}
 
-    
+function guardar(){
+    Swat.fire({
+    position: 'top—end' ,
+    icon: 'success',
+    title: 'Producto Agregado Con Éxito' ,
+    shovConfirmButton: false,
+    timer: 1500
+    })
+}
+
+function createListIten(name) {
+    const listItem = document.createElement (" li") ;
+    const heading = document.createElement ("span")
+    heading. textContent = name ;
+    const botonEliminar= document.createElement ("button");
+
+    listItem.classList.add("list-item");
+    botonEliminar.textContent = "X";
+    botonEliminar.id = "boton—elininar" ;
+    botonEliminar.addEventListener("click",onClickDelete) ;
+
+    listItem.appendChitd ( heading ) ;
+    listItem.appendChitd ( botonEliminar) ;
+    return listItem;
+}
+
+function onClickDelete(){
+    this.parentNode.remove();
+}
+
